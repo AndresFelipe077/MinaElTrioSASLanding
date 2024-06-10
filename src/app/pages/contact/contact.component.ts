5db26cd3d7a8e6c877e9323e3f729d430781a3cf
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2024-06-10 10:45:53 -0500

    Adding code to send contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ConsultationComponent } from '../../components/consultation/consultation.component';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    ConsultationComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Enviar el formulario
+      console.log(this.contactForm.value);
+    }
+  }
 
 }
